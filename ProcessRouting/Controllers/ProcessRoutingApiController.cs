30b68a67413ed2a8a5cd09d51ef0d6855ec2b504
helmiyaazahra <helmiyaazahra@gmail.com>
2025-03-12 09:49:18 +0700

    API Update

--- ProcessRouting/Controllers/ProcessRoutingApiController.cs
+++ ProcessRouting/Controllers/ProcessRoutingApiController.cs
@@ -37,6 +37,36 @@ namespace ProcessRouting.Controllers
             return Json(data);
         }
 
+        [HttpPut("ProcessRouting/UpdateData/{id}")]
+        public async Task<IActionResult> UpdateData(int id, [FromBody] ProcessRoutingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Data tidak valid" });
+            }
+
+            var existingData = await _context.ProcessRoutings.FindAsync(id);
+            if (existingData == null)
+            {
+                return NotFound(new { message = "Data tidak ditemukan" });
+            }
+
+            existingData.processType = model.processType; 
+            existingData.opNumber = model.opNumber;
+            existingData.workcenter = model.workcenter;
+            existingData.processDescriptionShort = model.processDescriptionShort;
+            existingData.processDescriptionLong = model.processDescriptionLong;
+            existingData.processSpec = model.processSpec;
+            existingData.processInstruction = model.processInstruction;
+            existingData.checkingChart = model.checkingChart;
+            existingData.kc = model.kc;
+            existingData.fixture = model.fixture;
+            existingData.notes = model.notes;
+
+
+            await _context.saveChangesAsync();
+            return Json(new { message = "Data berhasil diperbarui", data = existingData });
+        }
 
     }
 }
